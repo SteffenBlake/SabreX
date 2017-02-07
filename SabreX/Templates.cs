51259e914f2fc9ff57712922bc519e52d49a4a98
SteffenBlake <steffen.cole.blake@gmail.com>
2017-02-06 23:28:40 -0700

    SabreX-001: Created Object Templating

---
+++ SabreX/Templates.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SabreX
+{
+    class Templates
+    {
+        List<ObjectTemplate> TemplateList = new List<ObjectTemplate>();
+
+        public void InitializeTemplates()
+        {
+            TemplateList.AddRange( new List<ObjectTemplate>()
+            {
+                new ObjectTemplate()
+                {
+                    Name = "Chair",
+                    MaxParams = new Dictionary<string, int>()
+                    {
+                        {"Size", (int) Data.SizeEnum.Big},
+                        {"Texture", (int) Data.TextureEnum.Bumpy},
+                        {"Style", (int) Data.StyleEnum.Elegant}
+                    },
+                    MinParams = new Dictionary<string, int>()
+                    {
+                        {"Size", (int) Data.SizeEnum.Small},
+                        {"Texture", (int) Data.TextureEnum.Rough},
+                        {"Style", (int) Data.StyleEnum.Creepy}
+                    }
+                },
+
+                new ObjectTemplate()
+                {
+                    Name = "Table",
+                    MaxParams = new Dictionary<string, int>()
+                    {
+                        {"Size", (int) Data.SizeEnum.Big},
+                        {"Texture", (int) Data.TextureEnum.Bumpy},
+                        {"Style", (int) Data.StyleEnum.Elegant}
+                    },
+                    MinParams = new Dictionary<string, int>()
+                    {
+                        {"Size", (int) Data.SizeEnum.Small},
+                        {"Texture", (int) Data.TextureEnum.Rough},
+                        {"Style", (int) Data.StyleEnum.Creepy}
+                    }
+                },
+
+            });
+        }
+    }
+}
